5a04f7fa6614455fb92467bf0002f95a4eb3cd76
LeeJunHyeok <chocojh1223@naver.com>
2021-12-09 02:07:58 +0900

    4차최종
    코인 삭제

--- 3차발표분/background.py
+++ 3차발표분/background.py
@@ -3,18 +3,13 @@ from pico2d import *
 class BackGround:
     def __init__(self):
         self.spring = load_image('back_spring.png')
-        self.coin = load_image('coin.png')
         self.mushroom = load_image('mushroom.png')
 
     def update(self):
         pass
 
     def draw(self):
-        mushroom0 = 1       # 일단 ㅈㅁ
+        mushroom0 = 1
         self.spring.draw(0, 0)
-        for i in range(380, 480, 30):
-            self.coin.draw(i, 75)
-        for i in range(1180, 1260, 30):
-            self.coin.draw(i, 75)
         if mushroom0 == 1:
             self.mushroom.draw(1400, 70)
